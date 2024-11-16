6ba8f75a23c03816e81274db3a5c2ce4c9d52b77
moslem <moslemamiri82@gmail.com>
2024-11-16 20:27:36 +0330

    add book_update

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("books/", views.BookListView.as_view(), name="books"),
     path("create/", views.BookCreateView.as_view(), name="book_create"),
+    path("books/<int:pk>/update/", views.BookUpdateView.as_view(), name="book_update"),
     path("books/<int:pk>/", views.book_detail, name="book_detail"),
     path("category/", views.category, name="category"),
     path("category/<int:pk>", views.category_book, name="category_book"),
--- books/views.py
+++ books/views.py
@@ -19,6 +19,7 @@ def home(request):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 4
+    ordering = ["-datetime_created"]
     template_name = "books/book_list.html"
     context_object_name = "books"
 
@@ -62,4 +63,14 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Book
+    fields = ["title", "author", "description", "price", "image"]
+    template_name = "books/book_update.html"
+    success_url = reverse_lazy("book_detail")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
